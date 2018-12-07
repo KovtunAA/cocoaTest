e231cb5b9ecb324b3c6f13a8c68d162018d5bb36
KovtunAA <kovtun@attractgroup.com>
2018-12-07 15:11:14 +0200

    Initial Commit

---
+++ CocoaTest/CocoaTest.h
@@ -0,0 +1,19 @@
+//
+//  CocoaTest.h
+//  CocoaTest
+//
+//  Created by  Marta Sladkih on 12/7/18.
+//  Copyright © 2018  Marta Sladkih. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CocoaTest.
+FOUNDATION_EXPORT double CocoaTestVersionNumber;
+
+//! Project version string for CocoaTest.
+FOUNDATION_EXPORT const unsigned char CocoaTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaTest/PublicHeader.h>
+
+
